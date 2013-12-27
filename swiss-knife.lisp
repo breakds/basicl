6ab06751ee6459ab6a217a6531954528ed9119a0
Break Yang <BreakDS@gmail.com>
2013-12-26 23:04:49 -0500

    remove newlines

--- swiss-knife.lisp
+++ swiss-knife.lisp
@@ -123,21 +123,3 @@ used to denote the lambda itself"
              (error "bad value for #f: ~a" numarg))
            `(declare (optimize (speed ,numarg)
                                (safety ,(- 3 numarg))))))
-           
-                    
-                   
-
-
-
-
-
-
-
-          
-
-
-  
-  
-
-
-  
